3f4509a5fd541b59750168c61e9c61adcee31402
ha0himanshuarora <ha0himanshuarora@gmail.com>
2025-03-25 21:58:18 +0530

    Turned Logo Image to a link redirecting to home page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
     <>
       <nav className={show ? "navbar show_navbar" : "navbar"}>
         <div className="logo">
-          <img src="/logo.png" alt="logo" />
+          <Link to={"/"}><img src="/logo.png" alt="logo" /></Link>
         </div>
         <div className="links">
           <ul>
