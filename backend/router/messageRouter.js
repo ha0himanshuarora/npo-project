4e1fd58eb14e5d89783096efd85ad9779154a7da
ha0himanshuarora <ha0himanshuarora@gmail.com>
2025-03-27 12:35:29 +0530

    Remove all files

--- backend/router/messageRouter.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { postMessage } from "../controllers/messageController.js";
-
-const router = express.Router();
-
-router.post("/send", postMessage);
-
-export default router;
--- backend/server.js
+++
@@ -1,6 +0,0 @@
-import app from "./index.js";
-
-
-app.listen(process.env.PORT, () => {
-  console.log(`Server Listening At Port ${process.env.PORT}`);
-});
